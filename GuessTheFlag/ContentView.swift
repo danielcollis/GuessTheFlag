dd0808d8b391293e7d290393d41987e6dd2034db
danielcollis <daniel.collis13@gmail.com>
2025-02-07 11:07:26 -0500

    ui changes and now 6 answers

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"].shuffled()
-    @State private var correctAnswer = Int.random(in: 0...2)
+    @State private var correctAnswer = Int.random(in: 0...5)
     
     @State private var showingScore = false
     @State private var resultText = ""
@@ -20,41 +20,100 @@ struct ContentView: View {
         NavigationStack {
             ZStack {
                 LinearGradient(
-                    gradient: Gradient(colors: [Color(red: 1.0, green: 0.7, blue: 0.3), Color.orange]),
+                    gradient: Gradient(colors: [Color(red: 1.0, green: 0.7, blue: 0.3), Color.black]),
                                 startPoint: .top,
                                 endPoint: .bottom
                             )
                 .ignoresSafeArea()
                 
-                VStack(spacing: 30) {
+                VStack {
+                    Spacer()
+                    Text("Guess the Flag")
+                        .font(.largeTitle.weight(.bold))
                     Spacer()
                     
-                    VStack {
-                        Text("Tap the flag of")
-                        Text(countries[correctAnswer])
-                            .bold()
-                    }
-                    
-                    ForEach(0..<3) {number in
-                        Button {
-                            isAnswerCorrect(selectedIndex: number)
-                        } label: {
-                            Image(countries[number])
+                    VStack(spacing: 25) {
+                        VStack {
+                            Text("Tap the flag of")
+                                .foregroundStyle(.secondary)
+                                .font(.subheadline.weight(.heavy))
+                            Text(countries[correctAnswer])
+                                .font(.largeTitle.weight(.semibold))
                         }
+                        
+                        HStack {
+                            ForEach(0..<2) {number in
+                                Button {
+                                    isAnswerCorrect(selectedIndex: number)
+                                } label : {
+                                    Image(countries[number])
+                                        .resizable()
+                                        .scaledToFit()
+                                        .clipShape(.capsule)
+                                }
+                            }
+                        }
+                        .padding(.horizontal, 10)
+                        
+                        HStack {
+                            ForEach(2..<4) {number in
+                                Button {
+                                    isAnswerCorrect(selectedIndex: number)
+                                } label : {
+                                    Image(countries[number])
+                                        .resizable()
+                                        .scaledToFit()
+                                        .clipShape(.capsule)
+                                }
+                            }
+                        }
+                        .padding(.horizontal, 10)
+                        
+                        HStack {
+                            ForEach(4..<6) {number in
+                                Button {
+                                    isAnswerCorrect(selectedIndex: number)
+                                } label : {
+                                    Image(countries[number])
+                                        .resizable()
+                                        .scaledToFit()
+                                        .clipShape(.capsule)
+                                }
+                            }
+                        }
+                        .padding(.horizontal, 10)
+//                        ForEach(0..<4) {number in
+//                            Button {
+//                                isAnswerCorrect(selectedIndex: number)
+//                            } label: {
+//                                Image(countries[number])
+//                                    .clipShape(.capsule)
+//                                    .shadow(radius: 5)
+//                            }
+//                        }
+                        
+                        .alert(resultText, isPresented: $showingScore) {
+                            Button("Continue") {nextQuestion()}
+                        } message: {
+                            Text("Score: \(playerScore)")
+                        }
+                        
                     }
-                    .alert(resultText, isPresented: $showingScore) {
-                        Button("Continue") {nextQuestion()}
-                    } message: {
-                        Text("Score: \(playerScore)")
-                    }
+                    .frame(maxWidth: 350)
+                    .padding(.vertical, 20)
+                    .background(.ultraThinMaterial)
+                    .clipShape(.rect(cornerRadius: 20))
                     
                     Spacer()
+                    Spacer()
+                    
                     Text("Score: \(playerScore)")
+                        .foregroundStyle(Color(red: 1.0, green: 0.7, blue: 0.3))
+                        .font(.largeTitle.weight(.bold))
                     Spacer()
                 }
+                .padding()
             }
-            .navigationTitle("Guess the Flag")
-            .navigationBarTitleDisplayMode(.inline)
         }
     }
     func isAnswerCorrect(selectedIndex: Int) {
@@ -72,7 +131,7 @@ struct ContentView: View {
     
     func nextQuestion() {
         countries.shuffle()
-        correctAnswer = Int.random(in: 0...2)
+        correctAnswer = Int.random(in: 0...5)
     }
 }
 
